e2e3ca09758b32bfb4f702d877514ed5e438f230
Dmitry <vinnik.dmitry07@gmail.com>
2022-06-13 19:10:42 +0200

    ex4

--- exe4_pssm.py
+++ exe4_pssm.py
@@ -15,16 +15,38 @@ INT_TO_AA = {index: aa for index, aa in enumerate(ALPHABET)}
 GAP_INDEX = AA_TO_INT['-']
 
 
+def all_equal(lst):
+    return lst.count(lst[0]) == len(lst)
+
+
 class MSA:
-
     def __init__(self, sequences):
-        """
-        Initialize the MSA class with the provided list of sequences. Check the
-        sequences for correctness. Pre-calculate any statistics you seem fit.
+        if not (
+            len(sequences) > 0
+            and all_equal([len(s) for s in sequences])
+            and all(all(si in ALPHABET for si in s) for s in sequences)
+        ):
+            raise TypeError
 
-        :param sequences: List containing the MSA sequences.
-        """
-        pass
+        self.sequences = sequences
+        self.sequences_matrix = np.array(sequences, dtype=bytes).view('S1').reshape((len(sequences), -1))
+
+        self.r = np.apply_along_axis(lambda col: np.unique(col).size, axis=0, arr=self.sequences_matrix)
+
+        self.seqs_elems_entries = self.get_seqs_elems_entries()
+
+    def get_seqs_elems_entries(self, weights=None):
+        a = np.array(list(ALPHABET), dtype='S1')
+        seqs_elems_entries = np.empty((self.sequences_matrix.shape[1], a.size))
+
+        for i in range(seqs_elems_entries.shape[0]):
+            b = self.sequences_matrix[:, i]
+            idx_sorted = a.argsort()
+            idx = idx_sorted[np.searchsorted(a, b, sorter=idx_sorted)]
+            mask = a[idx] == b
+            seqs_elems_entries[i] = np.bincount(idx[mask], minlength=a.size, weights=weights)
+
+        return seqs_elems_entries
 
     def get_pssm(self, *, bg_matrix=None, beta=10, use_sequence_weights=False,
                  redistribute_gaps=False, add_pseudocounts=False):
@@ -52,8 +74,26 @@ class MSA:
         :return: PSSM as numpy array of shape (L x 20, dtype=numpy.int64).
                  L = ungapped length of the primary sequence.
         """
-        pssm = np.zeros((20, 20))
+        if bg_matrix is None:
+            bg_freq = 0.05
+        else:
+            bg_freq = np.sum(bg_matrix, axis=0)
 
+        non_gap_mask = self.sequences_matrix[0] != b'-'
+
+        if use_sequence_weights:
+            f_matrix = self.get_seqs_elems_entries(self.get_sequence_weights())
+        else:
+            f_matrix = self.seqs_elems_entries
+
+        pssm = f_matrix[non_gap_mask, :-1]
+
+        if redistribute_gaps:
+            pssm += f_matrix[non_gap_mask, -1:] * bg_freq
+
+        pssm /= pssm.sum(axis=1, keepdims=True)
+        pssm = 2 * np.log2(pssm / bg_freq)
+        pssm[np.isinf(pssm)] = -20
         return np.rint(pssm).astype(np.int64)
 
     def get_size(self):
@@ -64,7 +104,7 @@ class MSA:
         :return: Tuple of two integers. First element is the number of
                  sequences in the MSA, second element is the MSA length.
         """
-        return (-1, -1)
+        return self.sequences_matrix.shape
 
     def get_primary_sequence(self):
         """
@@ -74,7 +114,7 @@ class MSA:
 
         :return: String containing the ungapped primary sequence.
         """
-        return 'NOPE'
+        return self.sequences[0].replace('-', '')
 
     def get_sequence_weights(self):
         """
@@ -85,9 +125,14 @@ class MSA:
         :return: Numpy array (dtype=numpy.float64) containing the weights for
                  all sequences in the MSA.
         """
-        weights = np.zeros(10)
 
-        return weights.astype(np.float64)
+        a = np.array(list(ALPHABET), dtype='S1')
+        matrix_alphabet_indexes_sorted = np.searchsorted(np.sort(a), self.sequences_matrix)
+        matrix_alphabet_indexes = np.argsort(a)[matrix_alphabet_indexes_sorted]
+
+        s = np.take_along_axis(self.seqs_elems_entries, matrix_alphabet_indexes.T, axis=1)
+        w = 1 / (self.r[:, np.newaxis] * s)
+        return w[self.r != 1].sum(axis=0)
 
     def get_number_of_observations(self):
         """
@@ -95,6 +140,7 @@ class MSA:
 
         :return: Estimate of independent observation (dtype=numpy.float64).
         """
-        num_obs = np.int64(-1)
+        return np.mean(self.r)
 
-        return num_obs.astype(np.float64)
+
+# MSA(['SE-AN', 'SE-ES', 'SEVEN', 'SE-AS']).get_pssm(use_sequence_weights=True)
